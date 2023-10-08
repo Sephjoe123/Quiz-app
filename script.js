2aad05b79435917cf14f854d4dde26daaaa697d3
Bello Joseph <joexef87@gmail.com>
2023-10-08 19:27:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const Nextbtn = document.querySelector(".next-button");
 const previousBtn = document.querySelector(".previous-btn");
 const resultModal = document.querySelector(".result-modal");
 let numCount = 0;
-
+let Quecount = 0;
 function startGame() {
   const startBtn = document.querySelector(".start-btn");
   const rulesSection = document.querySelector(".rules-section");
@@ -67,50 +67,65 @@ function chooseAnswer(previousBtn) {
   }
 
   UpdateQuizQuestion(questionText, previousBtn);
+}function chooseAnswer(previousBtn) {
+  const questionText = document.querySelector(".question-text");
+  questionText.innerText = Questions[numCount].question;
+
+  // Set the text of each option button based on the current question
+  for (let i = 0; i < options.length; i++) {
+    options[i].innerText = Questions[numCount].options[i];
+    
+    // Add the click event listener for each option here
+    options[i].addEventListener("click", () => {
+      if (options[i].innerText === Questions[numCount].correctAnswer) {
+        options[i].style.color = "green";
+      }
+    });
+  }
+
+  UpdateQuizQuestion(questionText, previousBtn);
 }
 
-
-function UpdateQuizQuestion(questionText,previousBtn) {
+function UpdateQuizQuestion(questionText, previousBtn) {
+  // Reset the color of all options to their default
+  options.forEach((QuizOptions) => {
+    QuizOptions.style.color = "";
+  });
 
   Nextbtn.addEventListener("click", () => {
     if (numCount < Questions.length - 1) {
       numCount++;
       document.querySelector(".question-count").innerText = numCount + 1;
       questionText.innerText = Questions[numCount].question;
-      previousBtn.style.display = "none"
-      for (let i = 0; i < options.length; i++) {
-        options[i].innerText = Questions[numCount].options[i];
-        
-      }
-    
-    validateAnswers(numCount,options[0], options[1],options[2], options[3])
-    }
+      previousBtn.style.display = "none";
 
-    else if(numCount == 3){
+      // Set the text of each option button based on the current question
+      for (let i = 0; i < options.length; i++) {
+        let QuizOptions = options[i];
+        QuizOptions.innerText = Questions[numCount].options[i];
+        console.log(Questions[numCount].options)
+      }
+    } else if (numCount == 3) {
       quizContainer.style.display = "none";
       resultModal.classList.remove("none");
-
     }
- 
   });
-  
-}
-function validateAnswers(num, op1, op2, op3, op4) {
-  
-  
-  // Repeat this logic for op2, op3, and op4 if needed
 }
 
+        
+function validateAnswers(numCount,QuizOptions) {
+  options.forEach((QuizOptions) => {
+    QuizOptions.style.color = "";
+  });
+ QuizOptions.addEventListener("click", () =>{
+  if(QuizOptions.innerText === Questions[numCount].correctAnswer){
+    QuizOptions.style.color = "green"
+    console.log(Questions[numCount])
+  }
+ })
+}
 
 
-// function showResult(){
-//  const restartBtn = document.querySelector(".restart")
-//  restartBtn.addEventListener("click",() =>{
-//  resultModal.style.display = "none";
 
-//  quizContainer.style.display = "block";
-//  UpdateQuizQuestion()
-//  })
-// }
 
-// showResult()
+
