bf93ba393fdd3cc29f554443d9debed06f719439
Bello Joseph <joexef87@gmail.com>
2023-10-14 03:07:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ const Nextbtn = document.querySelector(".next-button");
 const previousBtn = document.querySelector(".previous-btn");
 const resultModal = document.querySelector(".result-modal");
 let numCount = 0;
-let Quecount = 0;
+let scoreCount = 0;
 
-
-function startGame(numCount) {
+function startGame() {
   const startBtn = document.querySelector(".start-btn");
   const rulesSection = document.querySelector(".rules-section");
   startBtn.addEventListener("click", () => {
@@ -20,10 +19,9 @@ function startGame(numCount) {
     }
   });
   proceed(rulesSection);
-
 }
 
-startGame()
+startGame();
 function proceed(rulesSection) {
   returnBtn.addEventListener("click", () => {
     if (rulesSection.classList.contains("active")) {
@@ -31,15 +29,14 @@ function proceed(rulesSection) {
       startModal.classList.remove("none");
       startModal.classList.add("active");
     }
-   
   });
 
   StartQuiz(rulesSection);
 
   previousBtn.addEventListener("click", () => {
-    if(quizContainer.style.display = "block"){
-      rulesSection.classList.add("active")
-      quizContainer.style.display = "none"
+    if ((quizContainer.style.display === "block")) {
+      rulesSection.classList.add("active");
+      quizContainer.style.display = "none";
     }
   });
 
@@ -49,14 +46,12 @@ function proceed(rulesSection) {
 function StartQuiz(rulesSection) {
   proceedBtn.addEventListener("click", () => {
     if (rulesSection.classList.contains("active")) {
-      console.log(numCount)
       rulesSection.classList.remove("active");
       quizContainer.style.display = "block";
-      showQuestion(numCount)
+      showQuestion(numCount);
     } else {
       quizContainer.style.display = "block";
     }
-
   });
 }
 
@@ -66,44 +61,79 @@ function chooseAnswer(previousBtn) {
   UpdateQuizQuestion(questionText, previousBtn);
 }
 
-
-
 function UpdateQuizQuestion(questionText, previousBtn) {
   Nextbtn.addEventListener("click", () => {
-    if (numCount < Questions.length - 1) {
+    if (numCount < Questions.length - 1 ) {
       numCount++;
       document.querySelector(".question-count").innerText = numCount + 1;
       questionText.innerText = Questions[numCount].question;
       previousBtn.style.display = "none";
-    
+    } 
+    else{
+      updateScore()
     }
-  else if(Questions.length == 1){
-    numCount = 0;
-  }
-    console.log(numCount)
-    showQuestion(numCount)
-
+    showQuestion(numCount);
+  
   });
 }
 
-function showQuestion (count){
-let option =`<div class = "quizbtn">` + Questions[count].options[0]  + `</div>` +
-`<div class = "quizbtn">` + Questions[count].options[1]  + `</div>` +
-`<div class = "quizbtn">` + Questions[count].options[2]  + `</div>` +
-`<div class = "quizbtn">` + Questions[count].options[3]  + `</div>` 
+function showQuestion(count) {
+  let option =
+    `<button class="quizbtn">` +
+    Questions[count].options[0] +
+    `</button>` +
+    `<button class="quizbtn">` +
+    Questions[count].options[1] +
+    `</button>` +
+    `<button class="quizbtn">` +
+    Questions[count].options[2] +
+    `</button>` +
+    `<button class="quizbtn">` +
+    Questions[count].options[3] +
+    `</button>`;
 
- document.querySelector(".quiz").innerHTML = option;
- validateAnswers(option)
+  document.querySelector(".quiz").innerHTML = option;
+  validateAnswers();
 }
 
-function validateAnswers(option) {
-const QuizOption = document.querySelectorAll(".quizbtn")
-for(let i = 0; i < QuizOption.length; i++){
-  QuizOption[i].addEventListener("click",(e) =>{
-    let answer = e.target;
-    if(answer.innerText == Questions[numCount].correctAnswer){
-      answer.style.backgroundColor = "green"
-    }
-  })
+function validateAnswers() {
+  let answer = Questions[numCount].correctAnswer;
+  const QuizOption = document.querySelectorAll(".quizbtn");
+
+  for (let i = 0; i < QuizOption.length; i++) {
+    let option = QuizOption[i];
+    option.addEventListener("click", (e) => {
+      let userChoice = e.target;
+
+      if (userChoice.innerText === answer) {
+        scoreCount++
+        userChoice.style.backgroundColor = "rgb(25,135,84)";
+        console.log(scoreCount)
+      } else if (userChoice.innerText !== answer) {
+        userChoice.style.backgroundColor = "rgb(237,67,55)";
+        userChoice.style.color = "#fff"
+      }
+
+      
+      QuizOption.forEach((option) => {
+        if (option.innerText === answer) {
+          option.style.backgroundColor = "rgb(25,135,84)";
+          option.style.color = "#fff"
+        }
+        option.disabled = true;
+      });
+    });
+  }
 }
+
+function updateScore(){
+  
+quizContainer.style.display = "none";
+resultModal.classList.remove("none");
+resultModal.classList.add("active");
+
+let ScoreValue = document.querySelector(".correct-question");
+ScoreValue.innerText = scoreCount;
+console.log(ScoreValue)
+
 }
