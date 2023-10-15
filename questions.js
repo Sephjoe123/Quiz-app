ddbbaba99b08e8d68df1402ed479d85e203313a7
Bello Joseph <joexef87@gmail.com>
2023-10-15 01:15:24 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,27 +1,77 @@
 const Questions = [
-   
-    {
-        question:  "What is the capital of Nigeria ?",
-        options: ["Benin Republic", "Ondo state", "Abuja", "Abeokuta"],
-        correctAnswer: "Abuja",
-    },
-
-    {
-        question: "What is the capital of France?",
-        options: ["Berlin", "Madrid", "Paris", "Rome"],
-        correctAnswer: "Paris",
-      },
-  
-    {
-      question: "Which planet is known as the Red Planet?",
-      options: ["Mars", "Venus", "Jupiter", "Saturn"],
-      correctAnswer: "Mars",
-    },
-
-    {
-      question: "Who wrote the play 'Romeo and Juliet'?",
-      options: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
-      correctAnswer: "William Shakespeare",
-    },
- 
-  ];
+  {
+    question: "What is the capital of Nigeria?",
+    options: ["Benin Republic", "Ondo state", "Abuja", "Abeokuta"],
+    correctAnswer: "Abuja",
+  },
+  {
+    question: "What is the capital of France?",
+    options: ["Berlin", "Madrid", "Paris", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    options: ["Mars", "Venus", "Jupiter", "Saturn"],
+    correctAnswer: "Mars",
+  },
+  {
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    options: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
+    correctAnswer: "William Shakespeare",
+  },
+  {
+    question: "What is the largest mammal in the world?",
+    options: ["African Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+    correctAnswer: "Blue Whale",
+  },
+  {
+    question: "Which gas do plants absorb from the atmosphere?",
+    options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+    correctAnswer: "Carbon Dioxide",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    correctAnswer: "Jupiter",
+  },
+  {
+    question: "Which gas makes up the majority of Earth's atmosphere?",
+    options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Argon"],
+    correctAnswer: "Nitrogen",
+  },
+  {
+    question: "In which country would you find the Great Barrier Reef?",
+    options: ["Australia", "Brazil", "Thailand", "South Africa"],
+    correctAnswer: "Australia",
+  },
+  {
+    question: "Who is known as the 'Father of Modern Physics'?",
+    options: ["Isaac Newton", "Marie Curie", "Albert Einstein", "Galileo Galilei"],
+    correctAnswer: "Albert Einstein",
+  },
+  {
+    question: "Which gas do plants release during photosynthesis?",
+    options: ["Oxygen", "Carbon Dioxide", "Methane", "Hydrogen"],
+    correctAnswer: "Oxygen",
+  },
+  {
+    question: "What is the largest ocean on Earth?",
+    options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+    correctAnswer: "Pacific Ocean",
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    options: ["Go", "Gd", "Ge", "Au"],
+    correctAnswer: "Au",
+  },
+  {
+    question: "Which planet is known as the 'Morning Star'?",
+    options: ["Mercury", "Venus", "Mars", "Jupiter"],
+    correctAnswer: "Venus",
+  },
+  {
+    question: "How many continents are there on Earth?",
+    options: ["3", "6", "7", "5"],
+    correctAnswer: "7",
+  },
+];
